676761eaf5bfca0f98b1f9cf0184755cb509ad7f
mosogbe <mohammed.sogbe@yahoo.com>
2023-12-31 20:42:47 +0000

    postgrs upgrade

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 import models
 import secrets
+import os
 from db import db
+from dotenv import load_dotenv 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
@@ -11,8 +13,10 @@ from resources.user import blp as UserBlueprint
 from blocklist import BLOCKLIST
 from flask_migrate import Migrate
 
+
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
@@ -21,7 +25,7 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
